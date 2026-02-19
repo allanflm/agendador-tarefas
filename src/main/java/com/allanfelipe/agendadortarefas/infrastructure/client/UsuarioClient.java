22987f024d0e7c96d458921852f8368379b343e3
Allan <allanfelipedk@gmail.com>
2026-02-19 14:41:53 -0300

    fix: correcao de apontamento

--- src/main/java/com/allanfelipe/agendadortarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/com/allanfelipe/agendadortarefas/infrastructure/client/UsuarioClient.java
@@ -2,7 +2,6 @@ package com.allanfelipe.agendadortarefas.infrastructure.client;
 
 import com.allanfelipe.agendadortarefas.business.dto.UsuarioDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "usuario", url = "${usuario.url}")
 public interface UsuarioClient {
 
-    @GetMapping
+    @GetMapping("/usuario")
     UsuarioDTO buscarUsuarioPorEmail(@RequestParam("email") String email,
                                      @RequestHeader("Authorization") String token
     );
